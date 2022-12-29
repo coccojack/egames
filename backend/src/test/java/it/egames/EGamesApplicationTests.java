fa146a57baf470ec55cd22c46eed0755e533ff9e
Giacomo Rotondo <grotondococco@gmail.com>
2022-12-29 18:48:46 +0100

    ➕ dependency update and entity init

--- backend/src/test/java/it/egames/EGamesApplicationTests.java
+++ backend/src/test/java/it/egames/EGamesApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EGamesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
