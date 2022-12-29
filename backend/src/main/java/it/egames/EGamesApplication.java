76ad24baa70d012502e484bf3106ea2818bc9d13
Giacomo Rotondo <grotondococco@gmail.com>
2022-12-29 18:02:16 +0100

    🎉 spring backend project begin

---
+++ backend/src/main/java/it/egames/EGamesApplication.java
@@ -0,0 +1,16 @@
+package it.egames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class EGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EGamesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/it/egames/EGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package it.egames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
