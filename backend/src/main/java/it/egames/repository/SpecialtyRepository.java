af04723c4c1b48d64703b78dbb9044ea1c1e472d
Giacomo Rotondo <grotondococco@gmail.com>
2023-01-18 01:42:25 +0100

    🗃️ repository update

--- backend/src/main/java/it/egames/repository/SpecialtyRepository.java
+++ backend/src/main/java/it/egames/repository/SpecialtyRepository.java
@@ -1,10 +1,9 @@
 package it.egames.repository;
 
 import it.egames.entity.Specialty;
-import it.egames.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SpecialtyRepository extends JpaRepository<Specialty,Long> {
+public interface SpecialtyRepository extends JpaRepository<Specialty, Long> {
 }
